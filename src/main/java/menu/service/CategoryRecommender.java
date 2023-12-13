62ee5579f1d95e9e95ee5c505feb3b372e9c599a
sohyun95277 <sohyun95277@gmail.com>
2023-12-13 17:27:48 +0900

    feat(CategoryRecommender): 추천에 성공한 카테고리들 저장

--- src/main/java/menu/service/CategoryRecommender.java
+++ src/main/java/menu/service/CategoryRecommender.java
@@ -1,35 +1,47 @@
 package menu.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import menu.repository.Category;
-
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import menu.repository.Category;
 
 public class CategoryRecommender {
     private static final int START_NUMBER = 1;
     private static final int END_NUMBER = 5;
     private static final int MAX_COUNT = 3;
     private Map<Category, Integer> counter = new HashMap<>();
+    private List<Category> recommend = new ArrayList<>();
 
     public Category recommend() {
         while (true) {
             int number = randomNumberGenerator();
-            Category category = Arrays.stream(Category.values())
-                    .filter(result -> result.getNumber() == number)
-                    .findFirst()
-                    .orElseThrow(() -> new IllegalArgumentException("[ERROR] 존재하지 않는 카테고리 번호입니다."));
-
+            Category category = getCategoryByNumber(number);
             int count = counter.getOrDefault(category, 0);
+
             if (count < MAX_COUNT) {
                 counter.put(category, count + 1);
+                recommend.add(category);
                 return category;
             }
         }
     }
 
+    private Category getCategoryByNumber(int number) {
+        Category category = Arrays.stream(Category.values())
+                .filter(result -> result.getNumber() == number)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 존재하지 않는 카테고리 번호입니다."));
+        return category;
+    }
+
     private int randomNumberGenerator() {
         return Randoms.pickNumberInRange(START_NUMBER, END_NUMBER);
+    }
+
+    public List<Category> getRecommend() {
+        return recommend;
     }
 }
