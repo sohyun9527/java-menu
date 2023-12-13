311014fa3a12dd291675975576a1c7e71ab74cf1
sohyun95277 <sohyun95277@gmail.com>
2023-12-13 17:24:24 +0900

    feat(MenuRecommender): valid한 추천 메뉴를 받을 때까지 재추천하는 기능

---
+++ src/main/java/menu/service/MenuRecommender.java
@@ -0,0 +1,46 @@
+package menu.service;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+import menu.domain.Coach;
+import menu.domain.Menu;
+import menu.repository.Category;
+import menu.repository.MenuBoard;
+
+public class MenuRecommender {
+
+    private final Category category;
+    private final List<Coach> coaches;
+
+    public MenuRecommender(Category category, List<Coach> coaches) {
+        this.category = category;
+        this.coaches = coaches;
+    }
+
+    public void recommendToCoaches() {
+        for (Coach coach : coaches) {
+            recommendUntilValid(coach);
+        }
+    }
+
+    private void recommendUntilValid(Coach coach) {
+        while (true) {
+            Menu recommend = generateMenu();
+            if (!coach.isAlreadyRecommended(recommend) && coach.isAlreadyHate(recommend)) {
+                coach.addRecommendMenu(recommend);
+                break;
+            }
+        }
+    }
+
+    // 한명의 코치에게 싫어하는 메뉴가 아니고, 이미 추천한 적 없다면
+    public Menu generateMenu() {
+        String menuName = recommend();
+        return Menu.of(menuName);
+    }
+
+    public String recommend() {
+        List<String> recommendList = MenuBoard.findByCategory(category);
+        return Randoms.shuffle(recommendList).get(0);
+    }
+}
