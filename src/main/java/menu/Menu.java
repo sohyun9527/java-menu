afea9ddab6bab07c15b6daa97578018760351374
sohyun95277 <sohyun95277@gmail.com>
2023-12-14 21:32:52 +0900

    feat(Menu): 존재하지 않는 메뉴 검증 기능

---
+++ src/main/java/menu/Menu.java
@@ -0,0 +1,23 @@
+package menu;
+
+import java.util.Arrays;
+import menu.repository.Category;
+import menu.repository.MenuBoard;
+
+public class Menu {
+    private final String name;
+    private final Category category;
+
+    public Menu(String name, Category category) {
+        this.name = name;
+        this.category = category;
+    }
+
+    public static Menu of(String name) {
+        return Arrays.stream(MenuBoard.values())
+                .filter(menuBoard -> menuBoard.getMenus().contains(name))
+                .findFirst()
+                .map(menuBoard -> new Menu(name, menuBoard.getCategory()))
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 존재하지 않는 메뉴입니다"));
+    }
+}
---
+++ src/main/java/menu/repository/Category.java
@@ -0,0 +1,25 @@
+package menu.repository;
+
+public enum Category {
+    JAPANESE("일식", 1),
+    KOREAN("한식", 2),
+    CHINESE("중식", 3),
+    ASIAN("아시안", 4),
+    WESTERN("양식", 5);
+
+    private final String name;
+    private final int number;
+
+    Category(String name, int number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}
