81f547736e664568da29c4be3f7f558c6c11da88
sohyun95277 <sohyun95277@gmail.com>
2023-12-14 23:32:48 +0900

    feat(MenuController): 흐름 초안 작성

---
+++ src/main/java/menu/controller/MenuController.java
@@ -0,0 +1,62 @@
+package menu.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import menu.domain.CategoryRecommender;
+import menu.domain.Coach;
+import menu.domain.Coaches;
+import menu.domain.Day;
+import menu.domain.Menu;
+import menu.domain.MenuRecommender;
+import menu.repository.Category;
+import menu.view.InputView;
+import menu.view.OutputView;
+
+public class MenuController {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public MenuController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+        outputView.printStartMessage();
+        String names = inputView.readCoachNames();
+        List<String> coachNames = List.of(names.split(",", -1));
+        Coaches coaches = getCoaches(coachNames);
+
+        for (Coach coach : coaches.getCoaches()) {
+            String menus = inputView.readHateMenus(coach.getName());
+            if (menus.isBlank()) {
+                continue;
+            }
+            List<String> hateMenus = List.of(menus.split(",", -1));
+            for (String hateMenu : hateMenus) {
+                coach.addHateMenu(Menu.of(hateMenu));
+            }
+        }
+        CategoryRecommender recommender = new CategoryRecommender();
+        MenuRecommender menuRecommender = new MenuRecommender();
+        outputView.printRecommendResultTitle();
+        outputView.printDays();
+        for (Day day : Day.values()) {
+            Category category = recommender.getValidCategoryRecommend();
+            for (Coach coach : coaches.getCoaches()) {
+                menuRecommender.recommendMenuUntilValid(coach, category);
+            }
+        }
+        outputView.printCategories(recommender.getRecommendCategories());
+        outputView.printRecommendDetail(coaches);
+        outputView.printClearMessage();
+    }
+
+    private Coaches getCoaches(List<String> coachNames) {
+        List<Coach> coaches = new ArrayList<>();
+        for (String coachName : coachNames) {
+            coaches.add(Coach.of(coachName));
+        }
+        return Coaches.of(coaches);
+    }
+}
