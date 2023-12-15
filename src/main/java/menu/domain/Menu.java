83e3fda73b03cee8e7c007922a5116a365b6de20
sohyun95277 <sohyun95277@gmail.com>
2023-12-15 16:28:38 +0900

    feat(Menu): 중복된 메뉴 이름 입력 시 검증

--- src/main/java/menu/domain/Menu.java
+++ src/main/java/menu/domain/Menu.java
@@ -1,6 +1,7 @@
 package menu.domain;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 public class Menu {
 
@@ -20,5 +21,22 @@ public class Menu {
                 .map(menuBoard -> new Menu(name, menuBoard.getCategory()))
                 .orElseThrow(() -> new IllegalArgumentException("[ERROR] 존재하지 않는 메뉴입니다."));
 
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Menu menu = (Menu) o;
+        return Objects.equals(name, menu.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
---
+++ src/main/java/menu/domain/Menus.java
@@ -0,0 +1,15 @@
+package menu.domain;
+
+import java.util.List;
+
+public class Menus {
+    private final List<Menu> menus;
+
+    public Menus(List<Menu> menus) {
+        this.menus = menus;
+    }
+
+    public static Menus of(String menuNames) {
+
+    }
+}
