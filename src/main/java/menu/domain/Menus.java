2d01ac4046d2469e793266e9468ba10e51367353
sohyun95277 <sohyun95277@gmail.com>
2023-12-15 16:31:42 +0900

    feat(Menu): 중복된 메뉴 이름 입력 시 검증

--- src/main/java/menu/domain/Menus.java
+++ src/main/java/menu/domain/Menus.java
@@ -1,8 +1,10 @@
 package menu.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Menus {
+    private static final String MENU_NAME_DELIMITER = ",";
     private final List<Menu> menus;
 
     public Menus(List<Menu> menus) {
@@ -10,6 +12,18 @@ public class Menus {
     }
 
     public static Menus of(String menuNames) {
+        List<String> names = List.of(menuNames.split(MENU_NAME_DELIMITER, -1));
+        List<Menu> menuList = new ArrayList<>();
+        for (String name : names) {
+            menuList.add(Menu.of(name));
+        }
+        validateDuplicateMenu(menuList);
+        return new Menus(menuList);
+    }
 
+    private static void validateDuplicateMenu(List<Menu> menuList) {
+        if (menuList.stream().distinct().count() != menuList.size()) {
+            throw new IllegalArgumentException("[ERROR] 중복된 메뉴가 존재합니다!");
+        }
     }
 }
