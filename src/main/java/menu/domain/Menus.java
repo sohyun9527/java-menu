9021ddd59dd8e3402c952b5b5d159220f07e818b
sohyun95277 <sohyun95277@gmail.com>
2023-12-15 16:38:21 +0900

    feat(Menus): 중복 검증 기능 추가

--- src/main/java/menu/domain/Menus.java
+++ src/main/java/menu/domain/Menus.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Menus {
+    private static final int MAX_COUNT = 3;
     private static final String MENU_NAME_DELIMITER = ",";
     private final List<Menu> menus;
 
@@ -11,15 +12,27 @@ public class Menus {
         this.menus = menus;
     }
 
-    public static Menus of(String menuNames) {
+    public static Menus generateHateMenus(String menuNames) {
         List<String> names = List.of(menuNames.split(MENU_NAME_DELIMITER, -1));
         List<Menu> menuList = new ArrayList<>();
         for (String name : names) {
             menuList.add(Menu.of(name));
         }
+        validateOverMenuCount(menuList);
         validateDuplicateMenu(menuList);
         return new Menus(menuList);
     }
+
+    public List<Menu> getMenus() {
+        return menus;
+    }
+
+    private static void validateOverMenuCount(List<Menu> menuList) {
+        if (menuList.size() > MAX_COUNT) {
+            throw new IllegalArgumentException("[ERROR] 싫어하는 메뉴는 3개까지 입력 가능합니다.");
+        }
+    }
+
 
     private static void validateDuplicateMenu(List<Menu> menuList) {
         if (menuList.stream().distinct().count() != menuList.size()) {
