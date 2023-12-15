ca035ea8063e86678fbc8c4765965b7a99aed5e5
sohyun95277 <sohyun95277@gmail.com>
2023-12-15 16:10:24 +0900

    feat(Menu): 존재하지 않는 메뉴를 입력할 경우 예외 발생

---
+++ src/main/java/menu/domain/Menu.java
@@ -0,0 +1,24 @@
+package menu.domain;
+
+import java.util.Arrays;
+
+public class Menu {
+
+    private final String name;
+
+    private final Category category;
+
+    public Menu(String name, Category category) {
+        this.name = name;
+        this.category = category;
+    }
+
+    public static Menu of(String name) {
+        return Arrays.stream(MenuBoard.values())
+                .filter(menuBoard -> menuBoard.getMenuNames().contains(name))
+                .findFirst()
+                .map(menuBoard -> new Menu(name, menuBoard.getCategory()))
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 존재하지 않는 메뉴입니다."));
+
+    }
+}
