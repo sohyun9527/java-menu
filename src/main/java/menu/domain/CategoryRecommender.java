a2ba186a310fa12ba6db7510f11c110d16923317
sohyun95277 <sohyun95277@gmail.com>
2023-12-15 14:00:28 +0900

    feat(CategoryRecommender): 올바른 추천을 할때까지 재추천 하는 기능

--- src/main/java/menu/domain/CategoryRecommender.java
+++ src/main/java/menu/domain/CategoryRecommender.java
@@ -1,21 +1,51 @@
 package menu.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class CategoryRecommender {
 
     private static final int START_NUMBER = 1;
     private static final int END_NUMBER = 5;
+    private static final int MAX_COUNT = 2;
+    private List<Category> recommended = new ArrayList<>();
+    private Map<Category, Integer> counter = new HashMap<>();
 
-    public Category recommend(int number) {
+    public Category recommend() {
+        while (true) {
+            int randomNumber = randomNumberGenerate();
+            Category category = getOneCategory(randomNumber);
+
+            if (!isOverRecommend(category)) {
+                updateCount(category);
+                return category;
+            }
+        }
+    }
+
+    private void updateCount(Category category) {
+        recommended.add(category);
+        counter.put(category, counter.getOrDefault(category, 0) + 1);
+    }
+
+    private boolean isOverRecommend(Category category) {
+        int count = counter.getOrDefault(category, 0);
+
+        return count > MAX_COUNT;
+    }
+
+    public Category getOneCategory(int number) {
         return Arrays.stream(Category.values())
                 .filter(value -> value.getNumber() == number)
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("[ERROR] 존재하지 않는 카테고리 번호입니다."));
     }
 
-    public int randomNumberGenerator() {
+    public int randomNumberGenerate() {
         return Randoms.pickNumberInRange(START_NUMBER, END_NUMBER);
     }
 }
